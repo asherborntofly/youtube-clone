0e6f43365141606222e08ab650b8da6d330e2cfb
AkashRanaPL <akash.rana2510@gmail.com>
2023-01-06 18:24:04 +0530

    Completed the Site

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Stack, Box } from "@mui/system"
+import {ChannelCard, VideoCard} from "./index"
 
-const Videos = () => {
+const Videos = ({videos, direction}) => {
+  if(!videos?.length) return 'Loading...';
+
   return (
-    <div>Videos</div>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item}/>}
+          {item.id.channelId && <ChannelCard channelDetail={item}/>}
+        </Box>
+      ))}
+    </Stack>
   )
 }
 
-export default Videos
+export default Videos;
